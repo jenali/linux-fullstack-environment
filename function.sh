1fcc797979e3297ef70f8f09bb8c1f7372bd7960
e.v.yolkin <e.v.yolkin@gmail.com>
2019-06-15 12:42:46 +0300

    Move functions.

---
+++ function.sh
@@ -0,0 +1,70 @@
+function question()
+{
+	local __question=$1
+    local __eval=$2
+	echo  $__question  "(y/n)?"
+	read answer
+    
+    if [[ "$answer" =~ ^([yY][eE][sS]|[yY])+$ ]]
+    then
+        echo "Y"
+        $__eval
+    fi
+
+}
+
+
+function updateSysytem() {
+    echo 'Start update & upgrade base system.'
+    apt-get update -y
+    apt-get upgrade -y
+    echo 'System is updated!'
+}
+
+function installGit()
+{
+    echo 'Install git...'
+    apt install git -y
+    git config --global user.name "e.v.yolkin"
+    git config --global user.email "e.v.yolkin@gmail.com"
+    git config core.fileMode false
+
+    git --version
+}
+
+
+function installPhp() {
+    echo 'Install php...'
+    apt-get install php7.2 -y
+    apt-get install php-pear \
+	php7.2-curl \
+	php7.2-dev \
+	php7.2-gd \
+	php7.2-mbstring \
+	php7.2-zip \
+	php7.2-mysql \
+	php7.2-xml -y
+    php -v
+
+    echo 'Install composer...'
+    sudo apt install composer -y
+
+}
+
+function installNode() {
+    sudo apt install build-essential checkinstall
+    sudo apt install libssl-dev
+    wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
+    source /etc/profile
+    nvm install 10.0
+    nvm use 10.0
+
+    # install nodejs modules
+    npm install -g yarn
+
+}
+
+function installDocker()
+{
+	sudo snap install docker -y
+}
--- install.sh
+++ install.sh
@@ -1,50 +1,22 @@
 #!/bin/bash
+source function.sh
+
 
 cd /tmp
 
 # update base system
-echo 'Start update & upgrade base system.'
-apt-get update -y
-apt-get upgrade -y
+question "Upade systems" updateSysytem
 
 # install base program
 
 # install git
-echo 'Install git...'
-apt install git -y
-git config --global user.name "e.v.yolkin"
-git config --global user.email "e.v.yolkin@gmail.com"
-git --version
-
+question "Upade systems" installGit
 
 # install php
-echo 'Install php...'
-apt-get install php7.2 -y
-apt-get install php-pear \
-	php7.2-curl \
-	php7.2-dev \
-	php7.2-gd \
-	php7.2-mbstring \
-	php7.2-zip \
-	php7.2-mysql \
-	php7.2-xml -y
-php -v
-
-# install composer
-echo 'Install composer...'
-sudo apt install composer -y
+question "Upade systems" installPhp
 
 # install node, npm, yarn
-sudo apt install build-essential checkinstall
-sudo apt install libssl-dev
-wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
-source /etc/profile
-nvm install 10.0
-nvm use 10.0
-
-# install nodejs modules
-npm install -g yarn
 
 
 # install docker
-sudo snap install docker
+question "Upade systems" installDocker
